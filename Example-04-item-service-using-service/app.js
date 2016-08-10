eacf925579a980f097c15ca7818067dd3211b0dd
Trevor Tuchten <trevor.tuchten@tntgenesis.com>
2016-08-09 20:43:15 -0700

    Rewrite

--- Example-04-item-service-using-service/app.js
+++ Example-04-item-service-using-service/app.js
@@ -15,21 +15,24 @@ function ItemService() {
     };
 }
 
-angular.module('notesApp', []).service('ItemService', [ItemService]).controller('MainCtrl', [function () {
-    var self = this;
-    self.tab = 'first';
-    self.open = function (tab) {
-        self.tab = tab;
-    };
-}]).controller('SubCtrl', ['ItemService', function (ItemService) {
-    var self = this;
-    self.list = function () {
-        return ItemService.list();
-    };
-    self.add = function () {
-        ItemService.add({
-            id: self.list().length + 1,
-            label: 'Item ' + self.list().length
-        });
-    };
-}]);
+angular.module('notesApp', [])
+    .service('ItemService', [ItemService])
+    .controller('MainCtrl', [function () {
+        var self = this;
+        self.tab = 'first';
+        self.open = function (tab) {
+            self.tab = tab;
+        };
+    }])
+    .controller('SubCtrl', ['ItemService', function (ItemService) {
+        var self = this;
+        self.list = function () {
+            return ItemService.list();
+        };
+        self.add = function () {
+            ItemService.add({
+                id: self.list().length + 1,
+                label: 'Item ' + self.list().length
+            });
+        };
+    }]);
